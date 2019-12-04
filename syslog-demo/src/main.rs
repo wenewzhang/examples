5b49a6b9a79852c7eca6d89e48cec4fa00394f0a
wenewzhang <wenewboy@gmail.com>
2019-12-04 21:32:33 +0800

    syslog

---
+++ syslog-demo/src/main.rs
@@ -0,0 +1,40 @@
+// extern crate syslog;
+//
+// use syslog::{Facility, Formatter3164};
+//
+// fn main() {
+//   let formatter = Formatter3164 {
+//     facility: Facility::LOG_USER,
+//     hostname: None,
+//     process: "myprogram".into(),
+//     pid: 42,
+//   };
+//
+//   match syslog::unix(formatter) {
+//     Err(e)         => println!("impossible to connect to syslog: {:?}", e),
+//     Ok(mut writer) => {
+//       writer.err("hello world").expect("could not write error message");
+//     }
+//   }
+// }
+extern crate syslog;
+#[macro_use]
+extern crate log;
+
+use syslog::{Facility, Formatter3164, BasicLogger};
+use log::{SetLoggerError, LevelFilter};
+
+fn main() {
+    let formatter = Formatter3164 {
+        facility: Facility::LOG_USER,
+        hostname: None,
+        process: "myprogram".into(),
+        pid: 0,
+    };
+
+    let logger = syslog::unix(formatter).expect("could not connect to syslog");
+    log::set_boxed_logger(Box::new(BasicLogger::new(logger)))
+            .map(|()| log::set_max_level(LevelFilter::Info));
+
+    info!("hello world");
+}
