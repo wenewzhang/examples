429d155d0d197245d2bca94c60b6ceeb278ef090
wenewzhang <wenewboy@gmail.com>
2020-01-14 14:21:41 +0800

    reference

---
+++ lifetime-example/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s);
+    println!("the first word is: {}", &word);
+    s.clear(); //
+
+    // println!("the first word is: {}", &word); //error
+}
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
